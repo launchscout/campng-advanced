efbb2e9991dfe0240b066fab21c0d0871ec780e4
Chris Nelson <chris@gaslightsoftware.com>
2014-06-02 18:39:05 -0400

    resource spec for creating candidates

--- spec/services/candidate_resource_spec.js
+++ spec/services/candidate_resource_spec.js
@@ -1,14 +1,12 @@
 describe("Candidate service spec", function() {
 
-  var CandidateSvc;
 
   beforeEach(function() {
     module("app");
   });
 
-  beforeEach(inject(function(Candidate, $httpBackend) {
+  beforeEach(inject(function($httpBackend) {
     this.$httpBackend = $httpBackend;
-    CandidateSvc = Candidate;
   }));
 
   afterEach(function() {
@@ -16,10 +14,29 @@ describe("Candidate service spec", function() {
     this.$httpBackend.verifyNoOutstandingExpectation();
   });
 
-  it("should find candidates", function() {
-    this.$httpBackend.expectGET('/candidates').respond([{name: "Bob"}]);
-    candidates = CandidateSvc.query();
-    this.$httpBackend.flush();
-    expect(candidates.length).toEqual(1);
+  describe("finding candidates", function() {
+    inject(function(Candidate) {
+      it("should find candidates", function() {
+        this.$httpBackend.expectGET('/candidates').respond([{name: "Bob"}]);
+        candidates = Candidate.query();
+        this.$httpBackend.flush();
+        expect(candidates.length).toEqual(1);
+      });
+    });
+  });
+
+  describe("creating new candidates", function() {
+
+    inject(function(Candidate) {
+      it("should find candidates", function() {
+        this.$httpBackend.expectPOST('/candidates').respond({name: "Bob", id: 1});
+        candidate = new Candidate();
+        candidate.name = "Bob";
+        candidate.$save();
+        this.$httpBackend.flush();
+        expect(candidate.id).toEqual(1);
+      });
+    });
+
   });
 });
