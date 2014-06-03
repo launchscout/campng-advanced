cd1d6e2c291bb4c7015ae1eb154137f0a4692667
Chris Nelson <chris@gaslightsoftware.com>
2014-06-03 12:01:10 -0400

    new candidates show up using events with passing e2e spec

--- app/js/controllers/candidates_controller.js
+++ app/js/controllers/candidates_controller.js
@@ -1,4 +1,7 @@
-// with $resource
-angular.module("app").controller("CandidatesController", function (Candidate) {
+angular.module("app").controller("CandidatesController", function (Candidate, $rootScope) {
   this.candidates = Candidate.query();
+  var that = this;
+  $rootScope.$on("newCandidate", function(event, newCandidate) {
+    that.candidates.push(newCandidate);
+  });
 });
