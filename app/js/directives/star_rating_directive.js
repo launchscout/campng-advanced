0c4e38b041e4f4bec2baae9ee5f25389c0753341
Chris Nelson <chris@gaslightsoftware.com>
2014-06-03 17:55:36 -0400

    star rating with stars from candidate

--- app/js/directives/star_rating_directive.js
+++ app/js/directives/star_rating_directive.js
@@ -1,8 +1,14 @@
 angular.module("app").directive("starRating", function() {
   return {
+    scope: {
+      "rating": "="
+    },
     link: function(scope, element, attrs) {
       $(element).raty({
         path: "/img",
+      });
+      scope.$watch("rating", function(rating) {
+        $(element).raty("score", rating);
       });
     }
   }
