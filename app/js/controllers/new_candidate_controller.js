284addab86706c6c43ce53ddb9256d53e4846915
Chris Nelson <chris@gaslightsoftware.com>
2014-06-03 12:01:10 -0400

    new candidates show up using events with passing e2e spec

--- app/js/controllers/new_candidate_controller.js
+++ app/js/controllers/new_candidate_controller.js
@@ -1,9 +1,10 @@
-angular.module("app").controller("NewCandidateController", function (Candidate, $location) {
+angular.module("app").controller("NewCandidateController", function (Candidate, $location, $rootScope) {
   this.candidate = new Candidate();
 
   this.save = function() {
     this.candidate.$save(function(savedCandidate) {
       $location.path("/candidates/" + savedCandidate.id);
+      $rootScope.$broadcast("newCandidate", savedCandidate);
     });
-  }
+  };
 });
