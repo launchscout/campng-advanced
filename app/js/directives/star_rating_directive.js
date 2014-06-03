be0bb2848f938be73e3bd5037540e8ced32bd213
Chris Nelson <chris@gaslightsoftware.com>
2014-06-03 18:08:21 -0400

    working star rating directive

--- app/js/directives/star_rating_directive.js
+++ app/js/directives/star_rating_directive.js
@@ -6,6 +6,11 @@ angular.module("app").directive("starRating", function() {
     link: function(scope, element, attrs) {
       $(element).raty({
         path: "/img",
+        click: function(score, event) {
+          scope.$apply(function() {
+            scope.rating = score;
+          });
+        }
       });
       scope.$watch("rating", function(rating) {
         $(element).raty("score", rating);
