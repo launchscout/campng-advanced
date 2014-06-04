a853f7a8bdcdfce7c3b07641a86e318e2acebb9a
Chris Nelson <chris@gaslightsoftware.com>
2014-06-04 15:45:41 -0400

    ssn validation

---
+++ app/js/directives/ssn_directive.js
@@ -0,0 +1,18 @@
+angular.module("app").directive("ssn", function() {
+  var SSN_REGEX = /\d{3}-\d{2}-\d{4}/;
+  return {
+    require: "ngModel",
+    restrict: "A",
+    link: function(scope, element, attributes, modelCtrl) {
+      modelCtrl.$parsers.push(function(viewValue) {
+        if (SSN_REGEX.test(viewValue)) {
+          modelCtrl.$setValidity("ssn", true);
+          return viewValue;
+        } else {
+          modelCtrl.$setValidity("ssn", false);
+          return undefined;
+        }
+      });
+    }
+  };
+});
