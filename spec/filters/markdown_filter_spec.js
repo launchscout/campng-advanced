7c6a3a17220db34c9620c555c6016c17ce4656af
Chris Nelson <chris@gaslightsoftware.com>
2014-06-05 18:10:44 -0400

    correctly failing spec

--- spec/filters/markdown_filter_spec.js
+++ spec/filters/markdown_filter_spec.js
@@ -1,5 +1,7 @@
   describe('markdown', function() {
 
+    beforeEach(module("app"));
+    
     it('should markdown', inject(function(markdownFilter) {
       expect(markdownFilter('hi *there*')).toEqual('<p>hi <em>there</em></p>');
     }));
