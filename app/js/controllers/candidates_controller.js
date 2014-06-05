713054878135add3f08f805ad20ea0062729e275
Chris Nelson <chris@gaslightsoftware.com>
2014-06-05 00:41:27 -0400

    cleaning cruft

--- app/js/controllers/candidates_controller.js
+++ app/js/controllers/candidates_controller.js
@@ -2,8 +2,3 @@
 angular.module("app").controller("CandidatesController", function (Candidate) {
   this.candidates = Candidate.query();
 });
-
-// with $http
-// angular.module("app").controller("BooksController", function ($scope, BookService) {
-//   $scope.books = BookService.getBooks();
-// });
