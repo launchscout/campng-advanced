c1a269d06bdd495cbb79c4663073b5b7db68e357
Chris Nelson <chris@gaslightsoftware.com>
2014-06-06 16:04:53 -0400

    need some $sce love

--- spec/filters/markdown_filter_spec.js
+++ spec/filters/markdown_filter_spec.js
@@ -1,8 +1,8 @@
   describe('markdown', function() {
 
     beforeEach(module("app"));
-    
-    it('should markdown', inject(function(markdownFilter) {
-      expect(markdownFilter('hi *there*')).toEqual('<p>hi <em>there</em></p>');
+
+    it('should markdown', inject(function(markdownFilter, $sce) {
+      expect($sce.getTrustedHtml(markdownFilter('hi *there*'))).toEqual('<p>hi <em>there</em></p>');
     }));
   });
