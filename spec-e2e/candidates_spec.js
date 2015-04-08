047bcdb62a40aaa36f6dc6b898e166221294acdc
Chris Nelson <superchrisnelson@gmail.com>
2015-04-08 16:47:11 -0400

    updated for latest protractor

--- spec-e2e/candidates_spec.js
+++ spec-e2e/candidates_spec.js
@@ -1,18 +1,13 @@
-var protractor = require('protractor');
-require('protractor/jasminewd');
-
 describe('my angular app', function () {
-  var ptor;
 
   describe('visiting the list of candidates', function () {
-    ptor = protractor.getInstance();
 
     beforeEach(function () {
-      ptor.get('/');
+      browser.get('/');
     });
 
     it('should show me a list of books', function() {
-      firstRow = ptor.findElement(by.repeater("candidate in ctrl.candidates").row(0));
+      firstRow = element(by.repeater("candidate in ctrl.candidates").row(0));
 
       firstRow.getText().then(function(text) {
         expect(text).toEqual("John Doe")
